0d74471d7db615176b04d3056adc7a5ff9804e14
pedrohqa <130668972+pedrohqa@users.noreply.github.com>
2023-09-28 16:36:16 -0300

    p

---
+++ teste5.php
@@ -0,0 +1,24 @@
+<?php
+    $a = 2; 
+    $b = 4;
+    $c = 5;
+
+    //Resultado igual 22
+    $calculo1 = $a + $b * $c; 
+    echo $calculo1;
+
+    //Resultado igual 10
+    $caclulo2 = $b / $a * $c; 
+    echo $calculo2;
+
+    $d = true;
+    $e = false;
+    $f = true;
+
+    //Utilização de 2 operadores lógicos 
+    if ($d || $e && $f){
+
+    //Condicão
+
+    }
+?>
